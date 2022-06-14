2dfa14520163ffe52ff3992f56b21efc70ec8236
TQ White II <tq@justkidding.com>
2022-06-14 15:26:50 -0500

    wrap/unwrap work but still only write files, not return data

--- qtools-secure-container.js
+++ qtools-secure-container.js
@@ -1,45 +1,59 @@
 #!/usr/bin/env node
 'use strict';
 
-const moduleName=__filename.replace(__dirname+'/', '').replace(/.js$/, ''); //this just seems to come in handy a lot
+const moduleName = __filename.replace(__dirname + '/', '').replace(/.js$/, ''); //this just seems to come in handy a lot
 
 const qt = require('qtools-functional-library');
 //console.dir(qt.help());
 
-// const qtoolsGen = require('qtools');
-// const qtools = new qtoolsGen(module, { updatePrototypes: true });
+const wrapGen = require('./lib/wrap');
+const unwrapGen = require('./lib/unwrap');
+const swapKeyGen = () => ({});
 
-console.error(`HELLO FROM: ${__filename}`);
-
-//npm i qtools-functional-library
-//npm i qtools-config-file-processor
-//npm i qtools-parse-command-line
-
-// const path=require('path');
-// const fs=require('fs');
-
-// const configFileProcessor = require('qtools-config-file-processor');
-//const config = configFileProcessor.getConfig('systemConfig.ini', __dirname)[__filename.replace(__dirname+'/', '').replace(/.js$/, '')];
-
-
-// const commandLineParser = require('qtools-parse-command-line');
-// const commandLineParameters = commandLineParser.getParameters();
-
+const nodeRsa = require('node-rsa');
 
 //START OF moduleFunction() ============================================================
 
-const moduleFunction = function(args={}) {
+const moduleFunction = function(args, callback) {
 
-	const workingFunctionActual=localArgs=>operatingArgs=>{
-	
-		return "hello";
-	
+	const { privateKey, publicKey, newKey } = args;
+	let {nodeRsaKey}=args;
+
+	const noPrivateKeyWrap = (args, callback) => {
+		callback(
+			'No private key supplied to qtools-secure-container. unwrap() not allowed.'
+		);
+	};
+
+	let wrap;
+	let unwrap;
+	let swapKey;
+	let err = '';
+
+	if (nodeRsaKey) {
+		wrap = wrapGen({ nodeRsaKey }).wrap;
+		unwrap = unwrapGen({ nodeRsaKey }).unwrap;
+		swapKey = swapKeyGen({ nodeRsaKey, newKey }).swapKey;
+	} else if (!nodeRsaKey && privateKey) {
+		nodeRsaKey = nodeRsa(privateKey);
+		wrap = wrapGen({ nodeRsaKey }).wrap;
+		unwrap = unwrapGen({ nodeRsaKey }).unwrap;
+		swapKey = swapKeyGen({ nodeRsaKey, newKey }).swapKey;
+	} else if (!nodeRsaKey && publicKey) {
+		nodeRsaKey = nodeRsa(publicKey);
+		wrap = wrapGen({ nodeRsaKey }).wrap;
+		unwrap = noPrivateKeyWrap; //can wrap() a thing but not unwrap() it w/o private key
+		swapKey = swapKeyGen({ nodeRsaKey, newKey }).swapKey;
+	} else if (!nodeRsaKey) {
+		nodeRsaKey = nodeRsa({ b: 2048 });
+		wrap = wrapGen({ nodeRsaKey, returnNewKeys: true }).wrap;
+		unwrap = unwrapGen({ nodeRsaKey }).unwrap;
+		swapKey = swapKeyGen({ nodeRsaKey, newKey }).swapKey;
+	} else {
+		err = 'No keys supplied to qtools-secure-container';
 	}
-	
 
-	const workingFunction=workingFunctionActual(Object.assign({}, args));
-
-	return ({workingFunction});
+	callback(err, { wrap, unwrap, swapKey });
 };
 
 //END OF moduleFunction() ============================================================
